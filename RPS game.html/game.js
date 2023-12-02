aedfc503c6c26684a30daf7e2cb1b58c866c9c03
Peter David <Princewill016@gmail.com>
2023-12-02 01:02:38 +0100

    completed the buttons

--- RPS game.html/game.js
+++ RPS game.html/game.js
@@ -1,16 +1,69 @@
+const player_played = document.getElementById("player_played");
+const computer_played = document.getElementById("computer_played");
+const player_result = document.querySelector(".score_player");
+const computer_result = document.querySelector(".score_computer");
+const whoWon = document.querySelector(".winner");
+
+let result = "";
 function Rock() {
   const playerChoice = "Rock";
-  choices = ["Rock", "Paper", "Scissors"];
+  //   const image1 = document.querySelector(".img1");
+  //   const image2 = document.querySelector(".img2");
+  //   const image3 = document.querySelector(".img3");
+  const choices = ["Rock", "Paper", "Scissors"];
   const computerChoice = choices[Math.floor(Math.random() * choices.length)];
   if (playerChoice === computerChoice) {
-    console.log("It's a tie!");
+    result = "It's a tie!";
   } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
-    console.log("You Win!");
-  } else console.log("You Lose!");
-  console.log(computerChoice);
-}
+    result = "You Won!";
+  } else result = "You Lose!";
+  player_played.textContent = `You chose ${"Rock"}`;
+  computer_played.textContent = `Computer chose ${computerChoice}`;
+  result === "You Won!" ? player_result.textContent++ : player_result;
+  result === "You Lose!" ? computer_result.textContent++ : computer_result;
+  //   image1.style.display = "block";
 
-// function displayResult(playerChoice, computerChoice, result) {
-//   const resultElement = document.getElementById("result");
-//   resultElement.textContent = `You chose ${playerChoice}. Computer chose ${computerChoice}. Result: ${result}`;
-// }
+  whoWon.textContent = result;
+  return result;
+}
+function Paper() {
+  const playerChoice = "Paper";
+  //   const image1 = document.querySelector(".img1");
+  //   const image2 = document.querySelector(".img2");
+  //   const image3 = document.querySelector(".img3");
+  const choices = ["Rock", "Paper", "Scissors"];
+  const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  if (playerChoice === computerChoice) {
+    result = "It's a tie!";
+  } else if (playerChoice === "Paper" && computerChoice === "Scissors") {
+    result = "You Lose!";
+  } else result = "You Won!";
+  player_played.textContent = `You chose ${"Paper"}`;
+  computer_played.textContent = `Computer chose ${computerChoice}`;
+  result === "You Won!" ? player_result.textContent++ : player_result;
+  result === "You Lose!" ? computer_result.textContent++ : computer_result;
+  //   image1.style.display = "block";
+
+  whoWon.textContent = result;
+  return result;
+}
+function Scissors() {
+  const playerChoice = "Scissors";
+  //   const image1 = document.querySelector(".img1");
+  //   const image2 = document.querySelector(".img2");
+  //   const image3 = document.querySelector(".img3");
+  const choices = ["Rock", "Paper", "Scissors"];
+  const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  if (playerChoice === computerChoice) {
+    result = "It's a tie!";
+  } else if (playerChoice === "Scissors" && computerChoice === "Rock") {
+    result = "You Lose!";
+  } else result = "You Won!";
+  player_played.textContent = `You chose ${"Scissors"}`;
+  computer_played.textContent = `Computer chose ${computerChoice}`;
+  result === "You Won!" ? player_result.textContent++ : player_result;
+  result === "You Lose!" ? computer_result.textContent++ : computer_result;
+  //   image1.style.display = "block";
+  whoWon.textContent = result;
+  return result;
+}
